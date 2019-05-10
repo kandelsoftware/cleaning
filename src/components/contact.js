63dddfa5a6d272cd49edba7175d5ea3791302be3
saroj <kandelsoftware@gmail.com>
2019-05-10 14:25:16 +0200

    change in files

--- src/components/contact.js
+++ src/components/contact.js
@@ -3,83 +3,21 @@ import React from "react";
 export default function Contact() {
   return (
     <div>
-      <section class="section-form" id="contact">
-        <div class="row">
-          <h2>We're happy to hear from you</h2>
-        </div>
-        <div class="row">
-          <form name="contact" netlify>
-            <div class="row">
-              <div class="col span-1-of-3">
-                <label for="name">Name</label>
-              </div>
-              <div class="col span-2-of-3">
-                <input
-                  type="text"
-                  name="name"
-                  id="name"
-                  placeholder="Your name"
-                  required
-                />
-              </div>
-            </div>
-            <div class="row">
-              <div class="col span-1-of-3">
-                <label for="email">Email</label>
-              </div>
-              <div class="col span-2-of-3">
-                <input
-                  type="email"
-                  name="email"
-                  id="email"
-                  placeholder="Your email"
-                  required
-                />
-              </div>
-            </div>
-            <div class="row">
-              <div class="col span-1-of-3">
-                <label for="find-us">How did you find us?</label>
-              </div>
-              <div class="col span-2-of-3">
-                <select name="find-us" id="find-us">
-                  <option value="friends" selected>
-                    Friends
-                  </option>
-                  <option value="search">Search engine</option>
-                  <option value="ad">Advertisement</option>
-                  <option value="other">Other</option>
-                </select>
-              </div>
-            </div>
-            <div class="row">
-              <div class="col span-1-of-3">
-                <label>Newsletter?</label>
-              </div>
-              <div class="col span-2-of-3">
-                <input type="checkbox" name="news" id="news" checked /> Yes,
-                please
-              </div>
-            </div>
-            <div class="row">
-              <div class="col span-1-of-3">
-                <label>Drop us a line</label>
-              </div>
-              <div class="col span-2-of-3">
-                <textarea name="message" placeholder="Your message" />
-              </div>
-            </div>
-            <div class="row">
-              <div class="col span-1-of-3">
-                <label>&nbsp;</label>
-              </div>
-              <div class="col span-2-of-3">
-                <button type="submit">Send it!</button>
-              </div>
-            </div>
-          </form>
-        </div>
-      </section>
+      <form name="contact" netlify>
+        <p>
+          <label>
+            Name <input type="text" name="name" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Email <input type="email" name="email" />
+          </label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
     </div>
   );
 }
